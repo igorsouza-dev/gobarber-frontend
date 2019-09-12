7864ad3e2c3fcf930e73d19b3b5f0a41cce691ba
Igor Souza <igorsouza.dev@gmail.com>
2019-09-12 06:31:46 -0300

    Adding pages layouts

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -2,20 +2,34 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
+import DefaultLayout from '../pages/_layouts/default';
+import AuthLayout from '../pages/_layouts/auth';
+
 export default function RouteWrapper({
   component: Component,
   isPrivate,
   ...rest
 }) {
   const signed = false;
-
   if (!signed && isPrivate) {
     return <Redirect to="/" />;
   }
   if (signed && !isPrivate) {
     return <Redirect to="/dashboard" />;
   }
-  return <Route {...rest} component={Component} />;
+
+  const Layout = signed ? AuthLayout : DefaultLayout;
+
+  return (
+    <Route
+      {...rest}
+      render={props => (
+        <Layout>
+          <Component {...props} />
+        </Layout>
+      )}
+    />
+  );
 }
 
 RouteWrapper.propTypes = {
