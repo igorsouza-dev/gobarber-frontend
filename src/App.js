ff67f929006d8ce2f2c1b117286bc4cd5a1844e5
Igor Souza <igorsouza.dev@gmail.com>
2019-09-12 06:40:08 -0300

    Added global style

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import { Router } from 'react-router-dom';
 import Routes from './routes';
 import history from './services/history';
 
+import GlobalStyle from './styles/globals';
+
 function App() {
   return (
     <Router history={history}>
       <Routes />
+      <GlobalStyle />
     </Router>
   );
 }
