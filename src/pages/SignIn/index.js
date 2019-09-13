6609a47a2310cdc00b722f24279ee533fa572a4c
Igor Souza <igorsouza.dev@gmail.com>
2019-09-13 06:31:24 -0300

    Added unform

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import { Form, Input } from '@rocketseat/unform';
 // import { Container } from './styles';
 import logo from '~/assets/logo.svg';
 
 export default function SignIn() {
+  function handleSubmit(data) {}
   return (
     <>
       <img src={logo} alt="GoBarber" className="src" />
-      <form>
-        <input type="email" placeholder="Type in your e-mail" />
-        <input placeholder="Type in your password" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Type in your e-mail" />
+        <Input name="password" placeholder="Type in your password" />
         <button type="submit">Login</button>
         <Link to="/register">
           Don&apos;t have an account?Click here to sign up.
         </Link>
-      </form>
+      </Form>
     </>
   );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 // import { Container } from './styles';
 import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
+  function handleSubmit(data) {}
   return (
     <>
       <img src={logo} alt="GoBarber" className="src" />
-      <form>
-        <input placeholder="Type in your name" />
-        <input type="email" placeholder="Type in your e-mail" />
-        <input placeholder="Type in your password" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Type in your name" />
+        <Input name="email" type="email" placeholder="Type in your e-mail" />
+        <Input name="password" placeholder="Type in your password" />
         <button type="submit">Create Account</button>
         <Link to="/">Already have an account? Click here to sign in.</Link>
-      </form>
+      </Form>
     </>
   );
 }
