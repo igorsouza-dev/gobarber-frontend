6b7a6407be9a5e16842b2309e4eb504480e32502
Igor Souza <igorsouza.dev@gmail.com>
2019-09-13 06:53:27 -0300

    Added form validation

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
-// import { Container } from './styles';
+import * as Yup from 'yup';
+
 import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email()
+    .required(),
+  password: Yup.string().required(),
+});
 
 export default function SignIn() {
   function handleSubmit(data) {}
   return (
     <>
       <img src={logo} alt="GoBarber" className="src" />
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="email" type="email" placeholder="Type in your e-mail" />
         <Input name="password" placeholder="Type in your password" />
         <button type="submit">Login</button>
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
-
+import * as Yup from 'yup';
 // import { Container } from './styles';
 import logo from '~/assets/logo.svg';
 
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email()
+    .required(),
+  password: Yup.string()
+    .min(6)
+    .required(),
+  name: Yup.string().required(),
+});
 export default function SignUp() {
   function handleSubmit(data) {}
   return (
     <>
       <img src={logo} alt="GoBarber" className="src" />
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="name" placeholder="Type in your name" />
         <Input name="email" type="email" placeholder="Type in your e-mail" />
-        <Input name="password" placeholder="Type in your password" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Type in your password"
+        />
         <button type="submit">Create Account</button>
         <Link to="/">Already have an account? Click here to sign in.</Link>
       </Form>
