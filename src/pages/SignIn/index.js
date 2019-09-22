569fd0e923a269a5edd624134efb97bdb65185fd
Igor Souza <igorsouza.dev@gmail.com>
2019-09-22 12:06:33 -0300

    Adding auth

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
+
+import { signInRequest } from '~/store/modules/auth/actions';
 
 import logo from '~/assets/logo.svg';
 
@@ -13,13 +16,20 @@ const schema = Yup.object().shape({
 });
 
 export default function SignIn() {
-  function handleSubmit(data) {}
+  const dispatch = useDispatch();
+  function handleSubmit({ email, password }) {
+    dispatch(signInRequest(email, password));
+  }
   return (
     <>
       <img src={logo} alt="GoBarber" className="src" />
       <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="email" type="email" placeholder="Type in your e-mail" />
-        <Input name="password" placeholder="Type in your password" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Type in your password"
+        />
         <button type="submit">Login</button>
         <Link to="/register">
           Don&apos;t have an account?Click here to sign up.
