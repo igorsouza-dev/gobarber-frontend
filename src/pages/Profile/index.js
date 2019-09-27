59c1b7832d8dbe13c339df60cf14418b8556ec55
Igor Souza <igorsouza.dev@gmail.com>
2019-09-27 06:37:15 -0300

    Adding the profile page

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Profile() {
-  return <h1>Profile</h1>;
+  const profile = useSelector(state => state.user.profile);
+  function handleSubmit(data) {}
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="email" placeholder="Type in your email" />
+        <Input name="name" placeholder="Type in your name" />
+        <hr />
+        <Input
+          name="password"
+          type="oldPassword"
+          placeholder="Your current password"
+        />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Your new password"
+        />
+        <Input
+          name="password"
+          type="confirmPassword"
+          placeholder="Confirm the new password"
+        />
+
+        <button type="submit">Update profile</button>
+      </Form>
+      <button type="button">Logout from GoBarber</button>
+    </Container>
+  );
 }
