1ec5809e261e1d1df41c20ef5fc5873721c82463
Igor Souza <igorsouza.dev@gmail.com>
2019-09-28 10:40:49 -0300

    Adding the dashboard's components

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+import api from '~/services/api';
 
-// import { Container } from './styles';
+import { Container, Time } from './styles';
 
 export default function Dashboard() {
-  return <h1>Dashboard</h1>;
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#fff" />
+        </button>
+        <strong>31/01</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Fulano</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>Free</span>
+        </Time>
+        <Time>
+          <strong>10:00</strong>
+          <span>Fulano</span>
+        </Time>
+        <Time>
+          <strong>11:00</strong>
+          <span>Fulano</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
